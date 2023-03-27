cf2e8fdfafd3053ae9d6293f18bee429f179fb98
HeshanKarunaratne <heshan947102@gmail.com>
2023-03-27 14:55:31 +0530

    Scheduler Unit tests

--- Scheduling Tasks/src/main/java/com/example/schedulingtasks/ScheduledTasks.java
+++ Scheduling Tasks/src/main/java/com/example/schedulingtasks/ScheduledTasks.java
@@ -18,7 +18,9 @@ public class ScheduledTasks {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() {
-        log.info("The time is now {}", dateFormat.format(new Date()));
+    public String reportCurrentTime() {
+        String a = "The time is now " + dateFormat.format(new Date());
+        log.info(a);
+        return a;
     }
 }
--- Scheduling Tasks/src/test/java/com/example/schedulingtasks/ScheduledTasksTest.java
+++ Scheduling Tasks/src/test/java/com/example/schedulingtasks/ScheduledTasksTest.java
@@ -1,14 +1,29 @@
 package com.example.schedulingtasks;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ScheduledTasksTest {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Test
     public void reportCurrentTime() {
         ScheduledTasks scheduledtasks = Mockito.mock(ScheduledTasks.class);
         scheduledtasks.reportCurrentTime();
         Mockito.verify(scheduledtasks, Mockito.atLeast(1)).reportCurrentTime();
+    }
+
+    @Test
+    public void reportCurrentTimeTest() throws InterruptedException {
+        ScheduledTasks scheduledTasks = new ScheduledTasks();
+        scheduledTasks.reportCurrentTime();
+        Thread.sleep(5000);
+        String currentTime = dateFormat.format(new Date());
+        Assertions.assertEquals("The time is now " + currentTime, scheduledTasks.reportCurrentTime());
     }
 }

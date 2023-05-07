4a92509005eb03da24d0921855c28db35e6706ef
HeshanKarunaratne <heshan947102@gmail.com>
2023-05-07 12:20:32 +0530

    Multi thread

--- Multithreading2/completableFuture/src/main/java/com/example/completableFuture/service/EmployeeReminderService.java
+++ Multithreading2/completableFuture/src/main/java/com/example/completableFuture/service/EmployeeReminderService.java
@@ -4,6 +4,9 @@ import com.example.completableFuture.database.EmployeeDatabase;
 import com.example.completableFuture.dto.Employee;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 /**
@@ -11,13 +14,14 @@ import java.util.stream.Collectors;
  */
 public class EmployeeReminderService {
 
-    public void sendReminderToEmployee() {
-        CompletableFuture.supplyAsync(() -> {
+    public static Void sendReminderToEmployee() throws ExecutionException, InterruptedException {
+        Executor executor = Executors.newFixedThreadPool(10);
+        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.supplyAsync(() -> {
 
             System.out.println("FetchEmployee: " + Thread.currentThread().getName());
             return EmployeeDatabase.fetchEmployees();
 
-        }).thenApply(employees -> {
+        }, executor).thenApplyAsync(employees -> {
 
             System.out.println("FilterNewEmployee: " + Thread.currentThread().getName());
             return employees
@@ -25,7 +29,7 @@ public class EmployeeReminderService {
                     .filter(employee -> "TRUE".equalsIgnoreCase(employee.getNewJoiner()))
                     .collect(Collectors.toList());
 
-        }).thenApply(employees -> {
+        }, executor).thenApplyAsync(employees -> {
 
             System.out.println("FilterTrainingNotComplete: " + Thread.currentThread().getName());
             return employees
@@ -33,16 +37,29 @@ public class EmployeeReminderService {
                     .filter(employee -> "TRUE".equalsIgnoreCase(employee.getLearningPending()))
                     .collect(Collectors.toList());
 
-        }).thenApply(employees -> {
+        }, executor).thenApplyAsync(employees -> {
 
             System.out.println("GetEmails: " + Thread.currentThread().getName());
             return employees.stream().map(Employee::getEmail)
                     .collect(Collectors.toList());
 
-        });
+        }, executor).thenAcceptAsync(emails -> {
+
+            System.out.println("SendEmail: " + Thread.currentThread().getName());
+            emails.forEach(EmployeeReminderService::sendEmail);
+
+        }, executor);
+        return voidCompletableFuture.get();
     }
 
-    public static void main(String[] args) {
+    public static void sendEmail(String email) {
+        System.out.println("Sending training reminder email to: " + email);
+    }
 
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        long start = System.currentTimeMillis();
+        sendReminderToEmployee();
+        long end = System.currentTimeMillis();
+        System.out.println("Time executed: " + (end - start) + "ms");
     }
 }

0cd51fb91f144357341c84247c97331cd1fd549e
HeshanKarunaratne <heshan947102@gmail.com>
2023-05-31 08:43:27 +0530

    App working without authentication

--- Microservices Swiggy App/swiggy-service/src/main/java/com/example/swiggyservice/controller/SwiggyAppController.java
+++ Microservices Swiggy App/swiggy-service/src/main/java/com/example/swiggyservice/controller/SwiggyAppController.java
@@ -2,6 +2,7 @@ package com.example.swiggyservice.controller;
 
 import com.example.swiggyservice.dto.OrderResponseDTO;
 import com.example.swiggyservice.service.SwiggyAppService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/swiggy")
+@Slf4j
 public class SwiggyAppController {
 
     @Autowired
@@ -25,6 +27,7 @@ public class SwiggyAppController {
 
     @GetMapping("/{orderId}")
     public OrderResponseDTO checkOrderStatus(@PathVariable String orderId) {
+        log.info("inside swiggy controller");
         return service.checkOrderStatus(orderId);
     }
 }
